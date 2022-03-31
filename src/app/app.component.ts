5cc1a8fa837b5a2cea153ad0d7ba1f3dd4072c94
Your Name <sifunadone4704@gmail.com>
2022-03-31 19:11:50 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,98 @@
-import { Component } from '@angular/core';
+
+/*
+https://www.eduforbetterment.com/
+*/
+import { Component, VERSION ,ViewChild } from '@angular/core';
+
+
+export class CsvData {
+    public id: any;
+    public min: any;
+    public max: any;
+    public score: any;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'uploadcsv';
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+  public records: any[] = [];
+  @ViewChild('csvReader') csvReader: any;
+  jsondatadisplay:any;
+
+  uploadListener($event: any): void {
+
+    let text = [];
+    let files = $event.srcElement.files;
+
+    if (this.isValidCSVFile(files[0])) {
+
+      let input = $event.target;
+      let reader = new FileReader();
+      reader.readAsText(input.files[0]);
+
+      reader.onload = () => {
+        let csvData = reader.result;
+        let csvRecordsArray = (<string>csvData).split(/\r\n|\n/);
+
+        let headersRow = this.getHeaderArray(csvRecordsArray);
+
+        this.records = this.getDataRecordsArrayFromCSVFile(csvRecordsArray, headersRow.length);
+      };
+
+      reader.onerror = function () {
+        console.log('error is occured while reading file!');
+      };
+
+    } else {
+      alert("Please import valid .csv file.");
+      this.fileReset();
+    }
+  }
+
+  getDataRecordsArrayFromCSVFile(csvRecordsArray: any, headerLength: any) {
+    let csvArr = [];
+
+    for (let i = 1; i < csvRecordsArray.length; i++) {
+      let curruntRecord = (<string>csvRecordsArray[i]).split(',');
+      if (curruntRecord.length == headerLength) {
+        let csvRecord: CsvData = new CsvData();
+        csvRecord.id = curruntRecord[0].trim();
+        csvRecord.min = curruntRecord[1].trim();
+        csvRecord.max = curruntRecord[2].trim();
+        csvRecord.score = curruntRecord[3].trim();
+        csvArr.push(csvRecord);
+      }
+    }
+    return csvArr;
+  }
+
+//check etension
+  isValidCSVFile(file: any) {
+    return file.name.endsWith(".csv");
+  }
+
+  getHeaderArray(csvRecordsArr: any) {
+    let headers = (<string>csvRecordsArr[0]).split(',');
+    let headerArray = [];
+    for (let j = 0; j < headers.length; j++) {
+      headerArray.push(headers[j]);
+    }
+    return headerArray;
+  }
+
+  fileReset() {
+    this.csvReader.nativeElement.value = "";
+    this.records = [];
+    this.jsondatadisplay = '';
+  }
+
+  getJsonData(){
+    this.jsondatadisplay = JSON.stringify(this.records);
+  }
+
+
 }
